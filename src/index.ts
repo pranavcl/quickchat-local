900f15444361782b90ecca701d208e9e99e1d2b3
Pranav Mishra <pranavcl@outlook.com>
2025-06-11 22:16:40 +0530

    Added per-IP rate limits

--- src/index.ts
+++ src/index.ts
@@ -43,25 +43,116 @@ dbConnect();
 // Socket.io config
 const io = new Server(server);
 
+// Rate limiting stuff
+
+const ipRateLimits: { [ip: string]: { lastRegister?: number; 
+	registerAttempts?: number; 
+	lastLogin?: number; 
+	loginAttempts?: number;
+	lastForgotPassword?: number;
+	lastTyping?: number;
+	lastMessage?: number;}
+} = {};
+
+const REGISTER_WINDOW = 10; // 10 minutes
+const REGISTER_MAX = 3;
+const LOGIN_WINDOW = 2; // 2 minutes
+const LOGIN_MAX = 10;
+const LAST_FORGOT_WINDOW = 3; // 3 minutes
+
+const TYPING_COOLDOWN = 0.5; // 0.5 seconds
+const MESSAGES_COOLDOWN = 0.5; // 0.5 seconds
+
 io.on("connection", (socket) => {
 	console.log("↩️ A user connected, ID: " + socket.id);
 	sockets.push(socket);
 
 	socket.data.loggedIn = false;
+	socket.data.ip = socket.handshake.address;
 
 	socket.on("login", (data) => {
+		const now = Date.now();
+		const entry = ipRateLimits[socket.data.ip] || {};
+
+		if(!entry.lastLogin || now - entry.lastLogin > LOGIN_WINDOW * 60 * 1000) {
+			entry.lastLogin = now;
+			entry.loginAttempts = 1;
+		} else {
+			entry.loginAttempts = (entry.loginAttempts ?? 0) + 1;
+		}
+
+		if (entry.loginAttempts > LOGIN_MAX) {
+			socket.emit("alert", {
+				type: "error",
+				message: "Too many login attempts. Please try again later.",
+				changeState: "login"
+			});
+			return;
+		}
+
+		ipRateLimits[socket.data.ip] = entry;
 		login(socket, data);
 	});
 
 	socket.on("register", (data) => {
+		const now = Date.now();
+		const entry = ipRateLimits[socket.data.ip] || {};
+
+		if(!entry.lastRegister || now - entry.lastRegister > REGISTER_WINDOW * 60 * 1000) {
+			entry.lastRegister = now;
+			entry.registerAttempts = 1;
+		} else {
+			entry.registerAttempts = (entry.registerAttempts ?? 0) + 1;
+		}
+
+		if (entry.registerAttempts > REGISTER_MAX) {
+			socket.emit("alert", {
+				type: "error",
+				message: "Too many registration attempts. Please try again later.",
+				changeState: "register"
+			});
+			return;
+		}
+
+		ipRateLimits[socket.data.ip] = entry;
 		register(socket, data);
 	});
 
 	socket.on("forgot_password", (data) => {
+		const now = Date.now();
+		const entry = ipRateLimits[socket.data.ip] || {};
+
+		if(!entry.lastForgotPassword || now - entry.lastForgotPassword > LAST_FORGOT_WINDOW * 60 * 1000) {
+			entry.lastForgotPassword = now;
+		} else {
+			socket.emit("alert", {
+				type: "error",
+				message: `You can only request a password reset once every ${LAST_FORGOT_WINDOW} minutes.`,
+				changeState: "forgot_password"
+			});
+			return;
+		}
+
+		ipRateLimits[socket.data.ip] = entry;
 		forgot_password(socket, data);
 	});
 
 	socket.on("typing", () => {
+		const now = Date.now();
+		const entry = ipRateLimits[socket.data.ip] || {};
+
+		if(!entry.lastTyping || now - entry.lastTyping > TYPING_COOLDOWN * 1000) { // 0.5 seconds cooldown
+			entry.lastTyping = now;
+		} else {
+			socket.emit("alert", {
+				type: "error",
+				message: "You are typing too fast. Please slow down.",
+				changeState: "chat"
+			});
+			return;
+		}
+
+		ipRateLimits[socket.data.ip] = entry;
 		typing(socket);
 	});
 
@@ -70,6 +161,21 @@ io.on("connection", (socket) => {
 	});
 
 	socket.on("send_message", (data) => {
+		const now = Date.now();
+		const entry = ipRateLimits[socket.data.ip] || {};
+
+		if(!entry.lastMessage || now - entry.lastMessage > MESSAGES_COOLDOWN * 1000) { // 0.5 seconds cooldown
+			entry.lastMessage = now;
+		} else {
+			socket.emit("alert", {
+				type: "error",
+				message: "You are sending messages too fast. Please slow down.",
+				changeState: "chat"
+			});
+			return;
+		}
+
+		ipRateLimits[socket.data.ip] = entry;
 		message(socket, data);
 	});
 
